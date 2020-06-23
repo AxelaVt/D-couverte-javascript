5e65c2cb711735f42525085157ab1ca70818c926
Alexa Vermenot <alexa.vermenot@orange.fr>
2020-06-23 13:14:19 +0200

    ennemi

--- filec.js
+++ filec.js
@@ -44,6 +44,12 @@ for(var i = 0; i < H_GRID; i++){
       block.setAttribute("class", "breakableWall");
       block.traverser = false;
     }
+    // else if (random100() > 96) {
+    //   block.style.backgroundColor = "red";
+    //   //block.style.backgroundImage = 'url("")';
+    //   block.setAttribute("class", "ennemi");
+    //   block.traverser = false;
+    // }
     else {
       block.style.backgroundColor = "green";  //sinon fond en vert on peut le traverser
       //block.style.backgroundImage = 'url("")';
@@ -58,23 +64,24 @@ for(var i = 0; i < H_GRID; i++){
 
   }
 }
-// blockGrid[0][0].style.backgroundColor = "green";
-// blockGrid[0][0].block.traverser = true;
-// blockGrid[1][1].style.backgroundColor = "green";
-// blockGrid[1][1].block.traverser = true;
-// blockGrid[1][0].style.backgroundColor = "green";
-// blockGrid[1][0].block.traverser = true;
+blockGrid[0][0].style.backgroundColor = "green";
+//blockGrid[0][0].block.traverser = true;
+blockGrid[1][1].style.backgroundColor = "green";
+//blockGrid[1][1].block.traverser = true;
+blockGrid[1][0].style.backgroundColor = "green";
+//blockGrid[1][0].block.traverser = true;
 
-var ennemi = [];
+var blockEnnemi = [];
 for(var i = 0; i < 10; i++){  //10 = nb d'ennemi à créer
-    ennemi.push([]);
-    let blockEnnemi = document.createElement("div");
-    blockEnnemi.style.width = "40px";
-    blockEnnemi.style.height = "40px";
-    blockEnnemi.style.display = "flex";
-    blockEnnemi.style.position = "absolute";
-    blockEnnemi.style.backgroundColor = "red";
-    blockEnnemi.setAttribute("class", "ennemi");
+    blockEnnemi.push([]);
+    let ennemi = document.createElement("div");
+    ennemi.style.width = "40px";
+    ennemi.style.height = "40px";
+    ennemi.style.display = "flex";
+    ennemi.style.position = "absolute";
+    ennemi.style.backgroundColor = "red";
+    //ennemi.style.backgroundImage = 'url("")';
+    ennemi.setAttribute("class", "ennemi");
 
     var styleEnnemi = ennemi.style;
     //generer deux coordonées aléatoires
@@ -82,91 +89,109 @@ for(var i = 0; i < 10; i++){  //10 = nb d'ennemi à créer
     posY = getRandomInt(WINDOW_HEIGHT - GRID_SIZE);
     console.log(posX);
     console.log(posY);
+    //recup les positions des walls et breackablewalls
+    var wallXY = document.getElementsByClassName("wall");
+    var breakableWallXY = document.getElementsByClassName("breakableWall");
+    // recup seulemnt des données de position
+    var wallX = [];
+    var wallY = [];
+    var breakableWallX = [];
+    var breakableWallY = [];
+    for(i = 0; i < wallXY.length; i++)	{
+    	wallX += " , " + wallXY[i].offsetLeft;
+      wallY += " , " + wallXY[i].offsetTop;
+    }
+    for(i = 0; i < breakableWallXY.length; i++)	{
+      breakableWallX += " , " + breakableWallXY[i].offsetLeft;
+      breakableWallY += " , " + breakableWallXY[i].offsetTop;
+    }
 
-    var wallX = document.getElementsByClassName("wall").offsetLeft;
-    var wallY = document.getElementsByClassName("wall").offsetTop;
-    var breakableWallX = document.getElementsByClassName("breakableWall").offsetLeft;
-    var breakableWallY = document.getElementsByClassName("breakableWall").offsetTop;
+    console.log(wallX);
+    console.log(wallY);
+    console.log(breakableWallX);
+    console.log(breakableWallY);
 
-
-      while (((posX + GRID_SIZE) === wallX) && ((posY + GRID_SIZE) === wallY)){
-        posX = getRandomInt(WINDOW_WIDTH - GRID_SIZE);
-        posY = getRandomInt(WINDOW_HEIGHT - GRID_SIZE);
+    // compare les données de position des wall et de l'ennemi
+    for (var i = 0; i < wallX.length; i++) {
+      wallX[i]
+     for (var i = 0; i < breakableWallX.length; i++) {
+      breakableWallX[i]
+        if ((wallX[i]- V_GRID <=  posX <= wallX[i]+ V_GRID) || (breakableWallX[i]- V_GRID <= posX <= breakableWallX[i]+ V_GRID)) {
+          posX = getRandomInt(WINDOW_WIDTH - GRID_SIZE);
+        }
+        posX = posX;
       }
-      posX = posX;
-      posY = posY;
+    }
 
-
-      while ( ((posX + GRID_SIZE) === breakableWallX) && ((posY + GRID_SIZE) === breakableWallY) ){
-        posX = getRandomInt(WINDOW_WIDTH - GRID_SIZE);
-        posY = getRandomInt(WINDOW_HEIGHT - GRID_SIZE);
+    for (var i = 0; i < wallY.length; i++) {
+      wallY[i]
+     for (var i = 0; i < breakableWallY.length; i++) {
+      breakableWallY[i]
+        if ((wallY[i]- H_GRID <=  posY <= wallY[i]+ H_GRID) || (breakableWallY[i]- H_GRID <= posY <= breakableWallY[i]+ H_GRID)){
+          posY = getRandomInt(WINDOW_HEIGHT - GRID_SIZE);
+        }
+        posY = posY;
       }
-      posX = posX;
-      posY = posY;
+    }
 
+    styleEnnemi.marginLeft = (posX).toString()+"px";
+    styleEnnemi.marginTop = (posY).toString()+"px";
+    document.getElementById("plateau").appendChild(ennemi);
 
-    blockEnnemi.style.marginLeft = (posX).toString()+"px";
-    blockEnnemi.style.marginTop = (posY).toString()+"px";
-    document.getElementById("plateau").appendChild(blockEnnemi);
-
-    ennemi[i].push(blockEnnemi);
-    console.log(ennemi);
+    //blockEnnemi[i].push(ennemi);
 
  }
 
-
-
-
-var frame = 0;
-function loop(){  //60x par seconde
-  //console.log("test");
-  if (frame === 60) {
-    switch(direction){
-      case "left":
-        if (yEnnemi > 0 && blockGrid[xEnnemi][yEnnemi-1].traverser)
-        yEnnemi--;
-        break;
-      case "right":
-        if (xEnnemi < H_GRID-1 && blockGrid[xEnnemi+1][yEnnemi].traverser)
-        xEnnemi++;
-        break;
-      case "up":
-        if (yEnnemi < V_GRID-1 && blockGrid[xEnnemi][yEnnemi+1].traverser)
-        yEnnemi++;
-        break;
-      case "down":
-        if (xEnnemi > 0 && blockGrid[xEnnemi-1][yEnnemi].traverser)
-        xEnnemi--;
-        break;
-    }
-
-    styleEnnemi.left = String(xEnnemi * GRID_SIZE) + 'px';
-    styleEnnemi.top = String(yEnnemi * GRID_SIZE) + 'px';
-
-    let random = random100();
-
-      if (random < 25) {
-        direction = "left";
-      }
-
-      if (random >= 25 && random < 50) {
-        direction = "right";
-      }
-
-      if (random >= 50 && random < 75) {
-        direction = "up";
-      }
-
-      if (random > 75) {
-        direction = "down";
-      }
-
-      frame = 0;
-    }
-  frame++;
-  window.requestAnimationFrame(loop);
-}
-window.requestAnimationFrame(loop);
+//
+// var frame = 0;
+// function loop(){  //60x par seconde
+//   if (frame === 60) {
+//     switch(direction){
+//       case "left":
+//         if (yEnnemi > 0 && blockGrid[xEnnemi][yEnnemi-1].traverser)
+//         yEnnemi--;
+//         break;
+//       case "right":
+//         if (xEnnemi < H_GRID-1 && blockGrid[xEnnemi+1][yEnnemi].traverser)
+//         xEnnemi++;
+//         break;
+//       case "up":
+//         if (yEnnemi < V_GRID-1 && blockGrid[xEnnemi][yEnnemi+1].traverser)
+//         yEnnemi++;
+//         break;
+//       case "down":
+//         if (xEnnemi > 0 && blockGrid[xEnnemi-1][yEnnemi].traverser)
+//         xEnnemi--;
+//         break;
+//     }
+//
+//     styleEnnemi.left = String(xEnnemi * GRID_SIZE) + 'px';
+//     styleEnnemi.top = String(yEnnemi * GRID_SIZE) + 'px';
+//
+//     let random = random100();
+//
+//       if (random < 25) {
+//         direction = "left";
+//       }
+//
+//       if (random >= 25 && random < 50) {
+//         direction = "right";
+//       }
+//
+//       if (random >= 50 && random < 75) {
+//         direction = "up";
+//       }
+//
+//       if (random > 75) {
+//         direction = "down";
+//       }
+//
+//       frame = 0;
+//     }
+//   frame++;
+//   window.requestAnimationFrame(loop);
+// }
+// window.requestAnimationFrame(loop);
 
 //pawn movement management
 document.onkeydown = function(event){
